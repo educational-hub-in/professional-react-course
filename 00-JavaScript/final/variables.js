b14e08741e1c785eb25fdecaf0f05d48a9b04540
Educational Hub <educationalhub.utube@gmail.com>
2024-09-20 00:14:19 +0530

    Create variables.js

---
+++ 00-JavaScript/final/variables.js
@@ -0,0 +1,25 @@
+// variables in js (const, let, var)
+
+const name = "santhosh";
+
+console.log(name);
+
+let age = 12;
+
+console.log(age);
+
+age = 21;
+
+console.log(age);
+
+var city = "Madurai";
+
+console.log(city);
+
+city = "Chennai";
+
+console.log(city);
+
+// console.log("My name is " + name + ". And My age is " + age );
+
+console.log(`My name is ${name}. And My age is ${age}. And I live in ${city}`);
