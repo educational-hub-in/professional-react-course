033295aa9bc355ed6f83aa043775785495fd1ffd
Educational Hub <educationalhub.utube@gmail.com>
2024-09-19 19:33:48 +0530

    Create sampleData.js

---
+++ 00-JavaScript/starter/sampleData.js
@@ -0,0 +1,82 @@
+const userProfile = [
+    {
+      userId: 1,
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      phone: "+1234567890",
+      preferences: {
+        theme: "dark",
+        notifications: true,
+        language: "en",
+      },
+      recentActivities: [
+        { type: "login", timestamp: "2024-08-28T12:00:00Z" },
+        { type: "update_profile", timestamp: "2024-08-27T15:30:00Z" },
+        { type: "purchase", timestamp: "2024-08-26T18:45:00Z" },
+      ],
+    },
+    {
+      userId: 2,
+      name: "Santhosh Kumar",
+      email: "santhosh.doe@example.com",
+      phone: "+9898989898",
+      preferences: {
+        theme: "light",
+        notifications: true,
+        language: "en",
+      },
+      recentActivities: [
+        { type: "login", timestamp: "2024-08-28T12:00:00Z" },
+        { type: "update_profile", timestamp: "2024-08-27T15:30:00Z" },
+        { type: "purchase", timestamp: "2024-08-26T18:45:00Z" },
+      ],
+    },
+    {
+      userId: 3,
+      name: "John Cena",
+      email: "john.cena@example.com",
+      phone: "+987654321",
+      preferences: {
+        theme: "dark",
+        notifications: true,
+        language: "en",
+      },
+      recentActivities: [
+        { type: "login", timestamp: "2024-08-28T12:00:00Z" },
+        { type: "update_profile", timestamp: "2024-08-27T15:30:00Z" },
+        { type: "purchase", timestamp: "2024-08-26T18:45:00Z" },
+      ],
+    },
+    {
+      userId: 4,
+      name: "William Smith",
+      email: "william.smith@example.com",
+      phone: "+1234567890",
+      preferences: {
+        theme: "dark",
+        notifications: true,
+        language: "en",
+      },
+      recentActivities: [
+        { type: "login", timestamp: "2024-08-28T12:00:00Z" },
+        { type: "update_profile", timestamp: "2024-08-27T15:30:00Z" },
+        { type: "purchase", timestamp: "2024-08-26T18:45:00Z" },
+      ],
+    },
+    {
+      userId: 5,
+      name: "Roman Reigns",
+      email: "roman.reing@example.com",
+      phone: "+1234567890",
+      preferences: {
+        theme: "light",
+        notifications: true,
+        language: "en",
+      },
+      recentActivities: [
+        { type: "login", timestamp: "2024-08-28T12:00:00Z" },
+        { type: "update_profile", timestamp: "2024-08-27T15:30:00Z" },
+        { type: "purchase", timestamp: "2024-08-26T18:45:00Z" },
+      ],
+    },
+  ];
